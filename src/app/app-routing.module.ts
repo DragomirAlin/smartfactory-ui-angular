86442edcfef2f58ed17f999db86ac2c7a95893fb
dragomiralin <dragomirdanielalin@yahoo.com>
2021-04-26 21:13:49 +0300

    add welcome template

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,16 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 import {HomeComponent} from './home/home.component';
+import {WelcomeComponent} from './welcome/welcome.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent}
+    {path: '', component: WelcomeComponent},
+    {path: 'home', component: HomeComponent}
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
