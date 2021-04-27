4bf845e87e83284169a4bc7da07456a95c8281b8
dragomiralin <dragomirdanielalin@yahoo.com>
2021-04-27 21:10:02 +0300

    add token service

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -4,14 +4,14 @@ import {HttpClient, HttpHeaders} from '@angular/common/http';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/map';
 import {environment} from '../../environments/environment';
+import {TokenService} from './token-service';
 
 @Injectable({
     providedIn: 'root'
 })
 export class AuthService {
 
-    constructor(
-        private http: HttpClient) {
+    constructor(private http: HttpClient, private tokenService: TokenService) {
     }
 
 
@@ -22,32 +22,25 @@ export class AuthService {
         params.append('client_secret', environment.clientSecret);
         params.append('redirect_uri', environment.redirectUri);
         params.append('code', code);
-        console.log('retrive token');
+
         const headers = new HttpHeaders({
             'Content-type': 'application/x-www-form-urlencoded; charset=utf-8',
             Authorization: 'Basic ' + btoa(environment.clientId + ':secret')
         });
         this.http.post(environment.openIdUrl, params.toString(), {headers})
             .subscribe(
-                data => this.saveToken(data),
+                data => this.tokenService.saveToken(data),
                 err => alert('Invalid Credentials')
             );
     }
 
-    saveToken(token): any {
-        const expireDate = new Date().getTime() + (1000 * token.expires_in);
-        Cookie.set('access_token', token.access_token, expireDate);
-        console.log('Obtained Access token');
-        window.location.href = environment.redirectUri;
-    }
-
     checkCredentials(): any {
-        return Cookie.check('access_token');
+        return this.tokenService.checkToken();
     }
 
     logout(): void {
-        const token = Cookie.get('access_token');
-        Cookie.delete('access_token');
+        const token = this.tokenService.getToken();
+        this.tokenService.deleteToken();
         window.location.href = environment.logoutUrl + token + '&post_logout_redirect_uri=' + environment.redirectUri;
     }
 }
