ed37b195450b7f2e1bcfc7da160ec0e904e260d1
dragomiralin <dragomirdanielalin@yahoo.com>
2021-06-03 21:39:02 +0300

    change welcome page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,16 +6,12 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 import {HttpClientModule} from '@angular/common/http';
-import { NavbarComponent } from './navbar/navbar.component';
-import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     WelcomeComponent,
-    NavbarComponent,
-    SidebarComponent
   ],
   imports: [
     BrowserModule,
