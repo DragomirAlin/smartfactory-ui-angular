b47c06bffd8e3bc90e0daede6c924cc2f2d6a069
dragomiralin <dragomirdanielalin@yahoo.com>
2021-06-09 21:15:57 +0300

    fix keycloak

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,36 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { WelcomeComponent } from './welcome/welcome.component';
+import {APP_INITIALIZER, NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {HomeComponent} from './home/home.component';
+import {WelcomeComponent} from './welcome/welcome.component';
 import {HttpClientModule} from '@angular/common/http';
 import {AuthService} from './auth/service/auth.service';
 import {AuthGuard} from './auth/guard/auth.guard';
+import {KeycloakAngularModule, KeycloakService} from 'keycloak-angular';
+import {initializeKeycloak} from './auth/init/keycloak-init.factory';
+import {ConfigInitService} from './auth/init/config-init.service';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent,
-    WelcomeComponent,
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule
-  ],
-  providers: [HttpClientModule, AuthService, AuthGuard],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        HomeComponent,
+        WelcomeComponent,
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        HttpClientModule,
+        KeycloakAngularModule,
+    ],
+    providers: [HttpClientModule, AuthService, AuthGuard, ConfigInitService,
+        {
+            provide: APP_INITIALIZER,
+            useFactory: initializeKeycloak,
+            multi: true,
+            deps: [KeycloakService, ConfigInitService],
+        }],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {AuthService} from '../auth/service/auth.service';
+import {KeycloakService} from 'keycloak-angular';
 
 @Component({
     selector: 'app-home',
@@ -10,19 +11,13 @@ export class HomeComponent implements OnInit {
     public isAuthenticated = false;
 
 
-    constructor(private authService: AuthService) {
+    constructor(private keycloakService: KeycloakService) {
     }
 
     ngOnInit(): void {
-        this.isAuthenticated = this.authService.isAuthenticated();
-        const i = window.location.href.indexOf('code');
-        if (!this.isAuthenticated && i !== -1) {
-            this.authService.retrieveToken(window.location.href.substring(i + 5));
-        }
     }
-
     logout(): void {
-        this.authService.logout();
+        this.keycloakService.logout();
     }
 
 }
